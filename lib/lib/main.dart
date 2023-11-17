526c852568e11c2ab7555919a2753c911eb64d71
ebrahemots <adminots@gmail.com>
2023-11-17 23:42:03 +0200

    foor loop

---
+++ lib/lib/main.dart
@@ -0,0 +1,14 @@
+void main() {
+  // int? n1 = 6;
+  // int n2 = 10;
+
+  // n1 = n1 ?? 15;
+  // print(n1 + n2);
+
+  //Loops in dart.
+  // for loop
+  List<String> names = ['ebrahem', 'asjraf', "ahmed"];
+  for (int i = 0; i <= names.length - 1; i++) {
+    print(names[i]);
+  }
+}
