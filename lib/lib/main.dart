efd6298990242db920b7d6d269258bfa8afa5114
ebrahemots <adminots@gmail.com>
2023-11-18 12:43:10 +0200

    added foor in loop

--- lib/lib/main.dart
+++ lib/lib/main.dart
@@ -16,9 +16,35 @@ void main() {
 
   //While loop
   //we can use while loop if we had a condition not how many times
-  int x = 0;
-  while (x <= 5) {
-    print("x = $x");
-    x++;
+  // int x = 0;
+  // while (x <= 5) {
+  //   print("x = $x");
+  //   x++;
+  // }
+
+  // //do while loop
+  // int y = 0;
+  // do {
+  //   print(y);
+  // } while (false);
+
+  // List<String> names = ['ebrahem', 'ashraf', 'ahmed'];
+  // for (var n in names) {
+  //   print(n);
+  // }
+
+  // Set<int> Degrees = {10, 20, 30};
+  // for (var D in Degrees) {
+  //   print(D);
+  // }
+
+  Map<String, int> StudentInformation = {
+    'Ebrahem': 25,
+    'Ahraf': 55,
+    'hamida': 40
+  };
+
+  for (MapEntry<String, int> Info in StudentInformation.entries) {
+    print('$Info.key $Info.value');
   }
 }
